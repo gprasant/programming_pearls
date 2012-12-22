7f5237d14c42e73ce780f60a8e009606474de19d
Prasanth Guruprasad <nagu89@gmail.com>
2012-12-22 09:13:45 +0530

    refactoring and checking in workspace files

--- src/go/binarySearch.go
+++ src/go/binarySearch.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 )
+
 var t int
 var x = []int{1, 2, 3, 4, 5}
+
 func main() {
-	
+
 	fmt.Println("Enter val to find ")
 	fmt.Scanf("%d", &t)
 	fmt.Println(BinarySearch(t, x))
@@ -15,13 +17,13 @@ func main() {
 func BinarySearch(t int, x []int) int {
 	l := 0
 	u := len(x) - 1
-	for ;l <= u; {
-		m := (l + u)/2
-		if(x[m] == t){
+	for l <= u {
+		m := (l + u) / 2
+		if x[m] == t {
 			return m
-		} else if(x[m] < t){
+		} else if x[m] < t {
 			l = m + 1
-		} else if(x[m] > t){
+		} else if x[m] > t {
 			u = m - 1
 		}
 	}
--- src/go/quicksort.go
+++ src/go/quicksort.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	x := []int{3, 9, 5, 2, 1}
 	quickSort(x, 0, 4)
 	fmt.Println("After sort", x)
@@ -11,22 +11,22 @@ func main(){
 func swap(slice []int, idx1 int, idx2 int) {
 	t := slice[idx1]
 	slice[idx1] = slice[idx2]
-	slice[idx2] = t;
+	slice[idx2] = t
 }
 
 func quickSort(slice []int, l int, u int) {
 	if l >= u {
 		return
-	} 
+	}
 	m := l
-	for i := l + 1; i<=u; i++{
-		if slice[i] < slice[m]{
+	for i := l + 1; i <= u; i++ {
+		if slice[i] < slice[m] {
 			m++
 			swap(slice, m, i)
 		}
 	}
 	swap(slice, l, m)
-	
+
 	quickSort(slice, l, m-1)
 	quickSort(slice, m+1, u)
 }
