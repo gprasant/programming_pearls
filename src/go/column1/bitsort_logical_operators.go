d7fae463dba931ac98103fba634b86004c367c21
Prasanth Guruprasad <nagu89@gmail.com>
2012-12-25 13:21:56 +0530

    added bitsort with logical operations

---
+++ src/go/column1/bitsort_logical_operators.go
@@ -0,0 +1,41 @@
+/*
+test - map[i/32] & (1 << (i & 1F))
+set  - map[i/32] |= (1 << (i & 1F))
+clr  - map[i/32] &^= (1 << (i & 1F)) [In Go, &^ is the bitwise clear operator]
+*/
+package main
+
+import "fmt"
+
+var N int = 8
+var bitmap = make([]int32, N/32+1)
+
+var WORD_LENGTH int32 = 32
+var MASK int32 = WORD_LENGTH - 1
+
+func main() {
+	var num int
+
+	for i := 0; i < N; i++ {
+		fmt.Scanf("%d\n", &num)
+		set(int32(num))
+	}
+
+	for i := 0; i < N; i++ {
+		if test(int32(i)) {
+			fmt.Println(i)
+		}
+	}
+}
+
+func test(i int32) bool {
+	return (bitmap[i/WORD_LENGTH]&(1<<uint((i&MASK))) != 0)
+}
+
+func set(i int32) {
+	bitmap[i/WORD_LENGTH] |= (1 << uint((i & MASK)))
+}
+
+func clr(i int32) {
+	bitmap[i/WORD_LENGTH] &^= (1 << uint((i & MASK)))
+}
